5705eae192a6cc89d9608f2800056e273a9a7125
RACollins <richardcollins128@gmail.com>
2025-09-05 15:32:00 +0900

    Refactor labour_value_in_bread.py to improve variable naming for clarity and consistency. Update .DS_Store to reflect recent project changes.

---
+++ gdp_consumption_uk_historical.py
@@ -0,0 +1,157 @@
+from manim import *
+import pandas as pd
+import os
+
+
+###################
+### Definitions ###
+###################
+
+### Uncomment when switching to WHITE background
+config.background_color = WHITE
+
+### Set default color for common objects
+Line.set_default(color=BLACK)
+Text.set_default(color=BLACK)
+Axes.set_default(color=BLACK)
+
+cwd = os.getcwd()
+
+#################
+### Functions ###
+#################
+
+
+def get_gdp_consumption_uk_historical_df() -> pd.DataFrame:
+    df = pd.read_csv(cwd + "/data/gdp_consumption_uk_historical.csv")
+    return df
+
+
+def make_axes(
+    x_range: list,
+    y_range: list,
+    x_numbers_to_include: list,
+    y_numbers_to_include: list,
+    log_x: bool,
+    log_y: bool,
+    x_length: int,
+    y_length: int,
+):
+    if log_y:
+        y_axis_config = {
+            "numbers_to_include": y_numbers_to_include,
+            "scaling": LogBase(custom_labels=True),
+        }
+    else:
+        y_axis_config = {
+            "numbers_to_include": y_numbers_to_include,
+        }
+    if log_x:
+        x_axis_config = {
+            "numbers_to_include": x_numbers_to_include,
+            "scaling": LogBase(custom_labels=True),
+        }
+    else:
+        x_axis_config = {
+            "numbers_to_include": x_numbers_to_include,
+        }
+    ax = Axes(
+        x_range=x_range,
+        y_range=y_range,
+        x_length=x_length,
+        y_length=y_length,
+        axis_config={
+            "color": BLACK,  # <- not needed if backgroud colour is default BLACK
+            "include_tip": False,
+            "include_numbers": True,
+            "decimal_number_config": {
+                "num_decimal_places": 0,
+                "group_with_commas": False,  # <- This removes the comma delimitation
+            },
+        },
+        x_axis_config=x_axis_config,
+        y_axis_config=y_axis_config,
+    )
+    ax.add_coordinates()
+    ax.coordinate_labels[0].set_color(BLACK)
+    ax.coordinate_labels[1].set_color(BLACK)
+    return ax
+
+
+###############
+### Classes ###
+###############
+
+
+##############
+### Scenes ###
+##############
+
+
+class ConsumptionVsGDP(Scene):
+
+    def construct(self):
+        ### Get data
+        df = get_gdp_consumption_uk_historical_df()
+
+    def generate_axes(
+        self,
+        x_range: list,
+        y_range: list,
+        x_numbers_to_include: list,
+        y_numbers_to_include: list,
+        log_x: bool,
+        log_y: bool,
+        animate_axes: bool,
+        x_axis_label: str,
+        y_axis_label: str,
+        font_size: int,
+        x_length: int,
+        y_length: int,
+        position: float = None,
+        scale: float = None,
+    ) -> tuple:
+        ax = make_axes(
+            x_range=x_range,
+            y_range=y_range,
+            x_numbers_to_include=x_numbers_to_include,
+            y_numbers_to_include=y_numbers_to_include,
+            log_x=log_x,
+            log_y=log_y,
+            x_length=x_length,
+            y_length=y_length,
+        )
+
+        if position:
+            ax = ax.move_to(RIGHT * position)
+        if scale:
+            ax = ax.scale(scale)
+
+        ### Add axis labels
+        x_label = ax.get_x_axis_label(
+            Text(x_axis_label, font_size=font_size, color=BLACK)
+        )
+        y_label = ax.get_y_axis_label(
+            Text(y_axis_label, font_size=font_size, color=BLACK)
+        )
+
+        if animate_axes:
+            ### Animate the creation of Axes
+            self.play(Write(ax))
+            self.play(Write(x_label))
+            self.play(Write(y_label))
+            self.wait()  # wait for 1 second
+        else:
+            ### Just generate without animation
+            self.add(ax)
+            self.add(x_label)
+            self.add(y_label)
+
+        return ax, x_label, y_label
+
+
+
+if __name__ == "__main__":
+    df = get_gdp_consumption_uk_historical_df()
+    print(df.head())
+    pass
